7f40a570a924a8ff9ce221b61dc29b95b075a85a
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-06-20 10:15:05 +0530

    feat: add checkboxes with labels

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,6 +1,27 @@
+"use client";
+
 import { FC } from "react";
 import { Button } from "./button";
-
 export const Menu: FC = () => {
-  return <Button />;
+  return (
+    <div className="flex items-center justify-center h-screen w-screen">
+      <div className="bg-white drop-shadow-lg p-10 space-y-5 rounded-md">
+        <label className="flex items-center justify-between">
+          <span>All Pages</span>
+
+          <input type="checkbox" className="form-checkbox" />
+        </label>
+        <div className="border-b" />
+        {["page1", "page2", "page3", "page4"].map((page: string, index) => (
+          <label key={index} className="flex items-center justify-between">
+            <span>{`Page ${index + 1}`}</span>
+            <input type="checkbox" className="form-checkbox" />
+          </label>
+        ))}
+        <div className="border-b" />
+        <div />
+        <Button />
+      </div>
+    </div>
+  );
 };
